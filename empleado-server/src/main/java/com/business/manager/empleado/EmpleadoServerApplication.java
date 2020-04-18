216ef3f4ad1164ad0d88d7288727d45e1682d515
Jonathan Perez <jonathan.perez.dev@gmail.com>
2020-04-18 16:30:54 -0500

    Implementation of config and discovery clients

--- empleado-server/src/main/java/com/business/manager/empleado/EmpleadoServerApplication.java
+++ empleado-server/src/main/java/com/business/manager/empleado/EmpleadoServerApplication.java
@@ -3,11 +3,15 @@ package com.business.manager.empleado;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.business.manager.empleado"})
 @EntityScan(basePackages = {"com.business.manager.empleado.dao"})
+@EnableDiscoveryClient
+@RefreshScope
 public class EmpleadoServerApplication {
 
 	public static void main(String[] args) {
